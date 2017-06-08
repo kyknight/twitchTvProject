c63377940fadd4bc588cfea134ac9a2936a63cdf
Kyleigh Knight <kyleigh.knight@hotmail.com>
2017-06-08 14:17:17 -0500

    style changes

--- twitchTV/js/app.js
+++ twitchTV/js/app.js
@@ -15,15 +15,15 @@ $(document).ready(function(){
                         url: "https://wind-bow.glitch.me/twitch-api/channels/"+channel,
                         success: function(data){
                             if(data.display_name == undefined){//not exist
-                                container.innerHTML +='<div class="row" id="undefined"><div class="col col-sm-4"><div class="row" ><div class="col col-sm-4"></div><div class="col-sm-4"><h2>'+channel+'<h2><p>This channel does not exist.</p></div></div></div></div>';      
+                                container.innerHTML +='<div class="row" id="undefined"><div class="col col-sm-6"><div class="row" ><div class="col col-sm-2"></div><div class="col-sm-4"><h2>'+channel+'<h2><p>This channel does not exist.</p></div></div></div></div>';      
                             }
                             else{
-                                container.innerHTML +='<div class="row" id="offline"><div class="col col-sm-4"><div class="row" ><div class="col col-sm-4"><img src="'+data.logo+'></div><div class="col col-sm-4"><h2>'+data.display_name+'<h2></div></div></div></div>'; 
+                                container.innerHTML +='<div class="row" id="offline"><div class="col col-sm-6"><div class="row" ><div class="col col-sm-2"><img src="'+data.logo+'></div><div class="col col-sm-4"><h2>'+data.display_name+'<h2><p>Channel is offline.</p></div></div></div></div>'; 
                             }
-                        }                    
+                        }                   
                     });                     
                 } else{//Online
-                    container.innerHTML +='<div class="row" id="online"><div class="col col-sm-4"><div class="row" ><div class="col col-sm-4"><img src="'+data.stream.channel.logo+'></div><div class="col col-sm-4"><h2>'+data.stream.channel.display_name+'<h2><p>'+data.stream.channel.status+'.</p><a href="'+data.stream.channel.url+'" target="_blank">Watch now!</a></div></div></div></div>';
+                    container.innerHTML +='<div class="row" id="online"><div class="col col-sm-6"><div class="row" ><div class="col col-sm-2"><img src="'+data.stream.channel.logo+'></div><div class="col col-sm-4"><h2>'+data.stream.channel.display_name+'<h2><p>'+data.stream.channel.status+'.</p><a href="'+data.stream.channel.url+'" target="_blank">Watch now!</a></div></div></div></div>';
                 } 
             },
             error: function (jqXHR, exception) {
